58aaad20d448eef0923dc71b4ebde2bf06f8fef5
22120433 <lequangvinh.127.2022@gmail.com>
2024-12-05 21:02:17 +0700

    recent order & best seller & total order & item in stock

--- ShoesShop/App.xaml.cs
+++ ShoesShop/App.xaml.cs
@@ -4,11 +4,9 @@ using Microsoft.UI.Xaml;
 
 using ShoesShop.Activation;
 using ShoesShop.Contracts.Services;
-using ShoesShop.Core.Contracts.Repository;
 using ShoesShop.Core.Contracts.Services;
 using ShoesShop.Core.Services.DataAcess;
 using ShoesShop.Core.Http;
-using ShoesShop.Core.Repository;
 using ShoesShop.Core.Services;
 using ShoesShop.Helpers;
 using ShoesShop.Models;
@@ -62,7 +60,6 @@ public partial class App : Application
 
             // Other Activation Handlers
 
-            // Http clients
             services.AddHttpClient("Backend", client =>
             {
                 var host = App.GetService<IStoreServerOriginService>().Host;
@@ -95,7 +92,6 @@ public partial class App : Application
             services.AddTransient<IShoesDataService, ShoesDataService>();
             services.AddTransient<IOrderDataService, OrderDataService>();
             services.AddSingleton<IStatisticDataService, StatisticDataService>();
-            services.AddSingleton<IStatisticRepository, StatisticRepository>();
             services.AddSingleton<IStoreServerOriginService, StoreServerOriginService>();
             
 
@@ -133,12 +129,7 @@ public partial class App : Application
 
         UnhandledException += App_UnhandledException;
     }
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddTransient<IStatisticDataService, StatisticDataService>();
-        // Các đăng ký dịch vụ khác
-    }
-
+    
 
     private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
     {
--- ShoesShop/Views/DashboardPage.xaml.cs
+++ ShoesShop/Views/DashboardPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.UI.Xaml.Controls;
-
 using ShoesShop.ViewModels;
+using Microsoft.UI.Xaml.Navigation;
 
 namespace ShoesShop.Views;
 
@@ -15,60 +15,17 @@ public sealed partial class DashboardPage : Page
     {
         ViewModel = App.GetService<DashboardViewModel>();
         InitializeComponent();
+        DataContext = ViewModel;
     }
 
-    private void TimeFrame_Checked(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    protected override async void OnNavigatedTo(NavigationEventArgs e)
     {
-        var radioButton = sender as RadioButton;
-        if (radioButton != null)
-        {
-            ViewModel.SelectedType = radioButton.Content.ToString() switch
-            {
-
-                "Day" => "day",
-                "Week" => "week",
-                "Month" => "month",
-                "Year" => "year",
-                _ => "day"
-            };
-        }
+        base.OnNavigatedTo(e);
+        await ViewModel.LoadDashboardDataCommand.ExecuteAsync(null);
     }
 
-    private void StartDate_DateChanged(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
-    {
-        PickDateError.Text = "";
 
-        var date = StartDate.Date;
 
-        var today = DateTimeOffset.Now;
-        var sevenDaysAgo = today.AddDays(-7);
 
-        if (date > today)
-        {
-            sender.Date = sevenDaysAgo;
-            PickDateError.Text = "The start day is not greater than today";
-            return;
-        }
 
-        ViewModel.StartDate = date.Value.DateTime;
-        ViewModel.IsDirty = true;
-    }
-
-    private void EndDate_DateChanged(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
-    {
-        PickDateError.Text = "";
-
-        var date = EndDate.Date;
-        var today = DateTimeOffset.Now;
-
-        if (date > today)
-        {
-            sender.Date = today;
-            PickDateError.Text = "The last date is not greater than today";
-            return;
-        }
-
-        ViewModel.LastDate = date.Value.DateTime;
-        ViewModel.IsDirty = true;
-    }
 }
